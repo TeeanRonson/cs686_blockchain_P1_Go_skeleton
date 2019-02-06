a4441c1a5bda4e7046d41f8ea6a40b7e92996e30
TeeanRonson <31168019+TeeanRonson@users.noreply.github.com>
2019-02-05 16:16:04 -0800

    Encoding

--- main.go
+++ main.go
@@ -7,37 +7,50 @@ import (
 
 func main() {
 
-	//String
-	//var str = "do"
-	//newStr := []uint8(str)
-	//var value = hex.EncodeToString(newStr)
-	//fmt.Println("Hex:", value)
+	fmt.Println("Testing the Get method")
+	//prefix := make([]uint8, 0)
+	//flagValue := p1.Flag_value{prefix, "Hello"}
+	//arr := make([]string, 0)
+	//nod := p1.Node{0, arr, flagValue}
+
+	//db := make(map[string]p1.Node)
+	//mpt := p1.MerklePatriciaTrie{db, "rootHash"}
+	//mpt.Get("dog")
 
 	//var hex_array = []uint8(value)
 
 	//fmt.Println("\nRun Compact Encode")
 	//fmt.Println("Result:", p1.Compact_encode(toInsert))
 
-	//-------------------
+	//-------------------//-------------------//-------------------//-------------------//-------------------
 
 	fmt.Println("Testing some values")
+	do := p1.EncodeToHex("do")
+	fmt.Println("Outside:", do);
 
-	//do := p1.EncodeToHex("do")
-	//fmt.Println(do) // This results in ASCII values of 6 4 6 F
 
+	//fmt.Println("-------------------")
 	//dog := p1.EncodeToHex("dog")
 	//puppy := p1.EncodeToHex("puppy")
 	//horse := p1.EncodeToHex("horse")
-	//fmt.Println(horse)
+	//fmt.Printf("dog: %s\n", dog)
 	//
 	//fmt.Println()
-	//fmt.Println("Compact Encode Values")
-	//fmt.Println(p1.Compact_encode(do))
+	//fmt.Println("\nCompact Encode Values")
 	//fmt.Println(p1.Compact_encode(dog))
+
+	//fmt.Println(p1.Compact_encode([]uint8 {6, 1}))
+	//fmt.Println(p1.Compact_encode([]uint8 {6, 4, 6, 15, 6, 7}))
 	//fmt.Println(p1.Compact_encode(puppy))
 	//fmt.Println(p1.Compact_encode(horse))
 
-	p1.Test_compact_encode()
+	//fmt.Println("\nTest Cases")
+	//a := p1.EncodeToHex("do")
+	//fmt.Printf("a: %x\n", a)
+	//
+	//fmt.Println("Output:", p1.Compact_encode(a))
+
+	//p1.Test_compact_encode()
 
 
 
--- p1/MerklePatriciaTrie.go
+++ p1/MerklePatriciaTrie.go
@@ -21,8 +21,15 @@ type Node struct {
 
 type MerklePatriciaTrie struct {
 	db map[string]Node
-	root string //root hash?
+	root string //root hash
+}
 
+/**
+Traverses the MPT to find the value associated with the key
+ */
+func getHelper(key string, mpt *MerklePatriciaTrie) string {
+
+	return ""
 }
 
 /*
@@ -31,6 +38,17 @@ iF the key doesnt exist, return an empty string
  */
 func (mpt *MerklePatriciaTrie) Get(key string) string {
 
+	//Create a path array
+	//Convert the key string into Hexcode to finds its path through the MPT
+	//Add each item of the Hexcode into the Path array
+	//pass MPT Tree and Key into the Helper method
+	path := make([]uint8, 0)
+	keyPath := EncodeToHex(key)
+	fmt.Println(keyPath)
+	for _, value := range keyPath {
+		path = append(path, value)
+	}
+	fmt.Println("path:", path)
 	return ""
 }
 
@@ -41,9 +59,6 @@ func (mpt *MerklePatriciaTrie) Insert(key string, new_value string) {
 
 	//flag := Flag_value{EncodeToHex(key), new_value}
 	//node := Node{2, nil, flag}
-
-
-
 }
 
 func (mpt *MerklePatriciaTrie) InsertHelp(node Node) {
@@ -65,17 +80,62 @@ func (mpt *MerklePatriciaTrie) Delete(key string) {
 /**
 Encodes the incoming Key(string) into Hex values
 
-Example: do --> 6746f in String form
+Example: do --> 646f in String form
 			--> [54 52 54 102] in ASCII form
+			--> for each value returned in the string we want to convert
+				that into values from 1-16: where f = 16
+			--> Algo:
+				for each value in 6 4 6 f
+					if value == number
+						add to list
+					else
+						convert letter to value
  */
-func EncodeToHex(key string) []byte {
+func EncodeToHex(key string) []uint8 {
 
-	src := []byte(key)
-	dst := make([]byte, hex.EncodedLen(len(src)))
-	hex.Encode(dst, src)
-	fmt.Printf("This is the dst value %s\n", dst)
-	return dst
+	//for each item in the key
+		//find its ASCII value
+		//Find the hex values associated with that ASCII
+		//Add it into the []uint8
+	result := make([]uint8, 0)
+	ascii := []byte(key)
+	fmt.Println(ascii)
+	for _, value := range ascii {
+		result = append(result, value/16)
+		result = append(result, value % 16)
+	}
 
+
+
+	//src := []uint8(key)
+	//dst := make([]uint8, hex.EncodedLen(len(src)))
+	//hex.Encode(dst, src)
+	//fmt.Printf("Hex value: %s\n", dst)
+	return result
+}
+
+func convertToChar1(letter string) int8 {
+
+	switch letter {
+	case "a":
+		return 10 //index
+	case "b":
+		return 11
+	case "c":
+		return 12
+	case "d":
+		return 13
+	case "e":
+		return 14
+	case "f":
+		return 15
+	}
+	return -1
+}
+
+func convertToChar2(letter string) int8 {
+
+	return -1
 }
 /*
 Function takes an array of HEX value as the input, mark the Node type(Branch, Leaf, Extension),
@@ -83,27 +143,22 @@ makes sure the length is even, and converts it into an array of ASCII numbers as
  */
 func Compact_encode(hex_array []uint8) []uint8 {
 	fmt.Println("Hex Array Original", hex_array)
-
-	//0 --> extension 1---> leaf node
+	//0 --> extension 1 --> leaf node
 	//If the last value is 16, it is a leaf node
 	term := 0
 	if hex_array[len(hex_array)-1] == 16 {
 		term = 1
 	}
-
-	fmt.Println("Term:", term)
 	//Remove the last two values i.e. 16
 	if term == 1 {
 		hex_array = hex_array[0: len(hex_array) - 1]
 	}
 	fmt.Println("Hex Array Modified", hex_array)
-
 	//create a new flags slice
 	flags := make([]uint8, 0)
 	oddlen := len(hex_array) % 2
 	flags = append(flags, uint8(2*term+oddlen))
 	fmt.Println("Flags", flags)
-
 	//If the length is odd
 	if oddlen == 1 {
 		hex_array = append(flags, hex_array...)
@@ -112,7 +167,6 @@ func Compact_encode(hex_array []uint8) []uint8 {
 		hex_array = append(flags, hex_array...)
 	}
 	fmt.Println("Hex Array with Odd check", hex_array)
-
 	//Convert result to 4 item length array
 	result := make([]uint8, 0)
 	for i:= 0; i < len(hex_array); i += 2 {
@@ -127,18 +181,13 @@ func Compact_encode(hex_array []uint8) []uint8 {
 Reverse the compact_encode function
  */
 func compact_decode(encoded_arr []uint8) []uint8 {
-
-	revert := make([]uint8, 0)
-
-
-
 	return []uint8{}
 }
 
 func Test_compact_encode() {
-	//fmt.Println(reflect.DeepEqual(compact_decode(Compact_encode([]uint8{1, 2, 3, 4, 5})), []uint8{1, 2, 3, 4, 5}))
+	fmt.Println(reflect.DeepEqual(compact_decode(Compact_encode([]uint8{1, 2, 3, 4, 5})), []uint8{1, 2, 3, 4, 5}))
 	//fmt.Println(reflect.DeepEqual(compact_decode(Compact_encode([]uint8{0, 1, 2, 3, 4, 5})), []uint8{0, 1, 2, 3, 4, 5}))
-	fmt.Println(reflect.DeepEqual(compact_decode(Compact_encode([]uint8{0, 15, 1, 12, 11, 8, 16})), []uint8{0, 15, 1, 12, 11, 8}))
+	//fmt.Println(reflect.DeepEqual(compact_decode(Compact_encode([]uint8{0, 15, 1, 12, 11, 8, 16})), []uint8{0, 15, 1, 12, 11, 8}))
 	//fmt.Println(reflect.DeepEqual(compact_decode(Compact_encode([]uint8{15, 1, 12, 11, 8, 16})), []uint8{15, 1, 12, 11, 8}))
 }
 
